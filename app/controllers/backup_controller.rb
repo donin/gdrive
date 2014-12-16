7118aab2f57342b027d1d538b3912cc9a55a7e67
Peter Donin <peter@donin.net>
2014-12-16 06:00:33 +0000

    ruby style guide =)

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -1,56 +1,45 @@
-# Backup controller
-# Sync backup folder with google drive cloud service
-require "rubygems"
-require "google/api_client"
+#!/usr/bin/ruby
+require 'rubygems'
+require 'google/api_client'
 require 'google/api_client/client_secrets'
 require 'google/api_client/auth/installed_app'
-require "google_drive"
+require 'google_drive'
 
+# Backup controller
+# Sync backup folder with google drive cloud service
 class BackupController < ApplicationController
-	def self.sync_backup
-		puts "Here I am!"
-	end
+  # Google API auth client
+  @client = nil
 
-# Authorizes with OAuth and gets an access token.
+  # Connecting to Google API via oAuth2
+  def self.auth
+    # Authorizes with OAuth and gets an access token.
+    # Initialize the client.
+    @client = Google::APIClient.new(
+      application_name: 'Backup application',
+      application_version: '0.1.0'
+    )
 
-# Initialize the client.
-client = Google::APIClient.new(
-	:application_name => 'Bakup application',
-	:application_version => '0.1.0'
-)
+    # Load p12 auth key file
+    @client.authorization = Signet::OAuth2::Client.new(
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      audience: 'https://accounts.google.com/o/oauth2/token',
+      scope: 'https://www.googleapis.com/auth/drive',
+      issuer: Rails.application.secrets.google_api_issuer,
+      signing_key: apikey
+    )
+    @client.authorization.fetch_access_token!
+  end
 
-# Load p12 auth key
-key = Google::APIClient::KeyUtils.load_from_pkcs12(
-	File.join(Rails.root,'config','google_client.p12'), 'notasecret')
+  # Sync backup folder with google drive backup folder
+  def self.sync_backup
+    # First of all auth via Google
+    auth
+  end
 
-client.authorization = Signet::OAuth2::Client.new(
-	:token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-	:audience => 'https://accounts.google.com/o/oauth2/token',
-	:scope => 'https://www.googleapis.com/auth/prediction',
-	:issuer => Rails.application.secrets.google_api_issuer,
-	:signing_key => key
-)
-client.authorization.fetch_access_token!
-puts "token: " + client.access_token
-
-#client = Google::APIClient.new
-#auth = client.authorization
-#auth.client_id = Rails.application.secrets.google_apiclient_id
-#auth.client_secret = Rails.application.secrets.google_api_client_secret
-#auth.scope =
-#	"https://www.googleapis.com/auth/drive" 
-	#                 auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
-	#                 print("1. Open this page:\n%s\n\n" % auth.authorization_uri)
-	#                 print("2. Enter the authorization code shown in the page: ")
-	#                 auth.code = $stdin.gets.chomp
-	#                 auth.fetch_access_token!
-	#                 access_token = auth.access_token
-	#
-	#                 # Creates a session.
-	#                 session = GoogleDrive.login_with_oauth(access_token)
-	#
-	#                 # Gets list of remote files.
-	#                 for file in session.files
-	#                   p file.title
-	#                   end
+  # Get p12 key file
+  def self.apikey
+    Google::APIClient::KeyUtils.load_from_pkcs12(
+      File.join(Rails.root, 'config', 'google_client.p12'), 'notasecret')
+  end
 end
