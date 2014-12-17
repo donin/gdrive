0dc3d59ae79a1e38afeb30a77a4f426ded347e07
Peter Donin <peter@donin.net>
2014-12-17 21:02:57 +0000

    Remove old remote files [in progress]

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -35,6 +35,7 @@ class BackupController < ApplicationController
       person: @secrets.gapi_sub
     )
     @client.authorization.fetch_access_token!
+    @drive = @client.discovered_api('drive', 'v2')
   end
 
   # Sync backup folder with google drive backup folder
@@ -42,14 +43,34 @@ class BackupController < ApplicationController
     # First of all auth via Google
     auth
 
-    # Get all files in the backup folder
-    backup_files.each do |file|
+    # Get all files in the local backup folder
+    local_files.each do |file|
       puts file.title
     end
   end
 
   # Upload backup files
   def upload_files
+  end
+
+  # Check remote folder limits
+  # Move old remote files to trash
+  def self.clear_old_remote_files
+    rfiles = remote_files # get all remote files
+    limit = @secrets.gdrive_files_limit
+    return if rfiles.count < limit
+    # Get candidates for delete
+    rfiles.sort_by! { |a| a.createdDate.to_i }.reverse![0..limit]
+    remove_files!(rfiles)
+  end
+
+  # Remove files from google drive directory
+  def self.remove_files!(files)
+    files.each do |f|
+      q = { fileId: f.id }
+      puts "remove file: #{f.title}"
+      @client.execute(api_method: @drive.files.trash, parameters: q)
+    end
   end
 
   # Get local backup file names
@@ -60,9 +81,10 @@ class BackupController < ApplicationController
   end
 
   # Get all files in the backup directory
-  def self.backup_files
-    @drive = @client.discovered_api('drive', 'v2')
-    q = { q: "'#{@secrets.gdrive_backup_folder}' in parents" }
+  def self.remote_files
+    q = { q: "'#{@secrets.gdrive_backup_folder}' in parents",
+          fields: 'items(createdDate,fileSize,id,mimeType,title)'
+    }
     api_result = @client.execute(api_method: @drive.files.list, parameters: q)
     api_result.data.items
   end
